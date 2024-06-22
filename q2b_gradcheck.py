05441ec7c53786d9a5f0d514615f89b7384c2138
Aver roes <tamerabdalrazaq@hotmail.com>
2024-06-22 16:22:45 +0300

    gradient checker

--- q2b_gradcheck.py
+++ q2b_gradcheck.py
@@ -11,8 +11,7 @@ def gradcheck_naive(f, x, gradient_text=""):
          loss and its gradients
     x -- the point (numpy array) to check the gradient at
     gradient_text -- a string detailing some context about the gradient computation
-    """
-
+"""
     rndstate = random.getstate()
     random.setstate(rndstate)
     fx, grad = f(x)  # Evaluate function value at original point
@@ -22,26 +21,21 @@ def gradcheck_naive(f, x, gradient_text=""):
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
+        x_plus_h = np.copy(x)
+        x_minus_h = np.copy(x)
+        x_plus_h[ix] += h
+        x_minus_h[ix] -= h
+        
+        random.setstate(rndstate)
+        f_plus_h, _ = f(x_plus_h)
+        
+        random.setstate(rndstate)
+        f_minus_h, _ = f(x_minus_h)
 
-        # Try modifying x[ix] with h defined above to compute numerical
-        # gradients (numgrad).
-
-        # Use the centered difference of the gradient.
-        # It has smaller asymptotic error than forward / backward difference
-        # methods. If you are curious, check out here:
-        # https://math.stackexchange.com/questions/2326181/when-to-use-forward-or-central-difference-approximations
-        # http://ufldl.stanford.edu/tutorial/supervised/DebuggingGradientChecking/
-
-        # Make sure you call random.setstate(rndstate)
-        # before calling f(x) each time. This will make it possible
-        # to test cost functions with built in randomness later.
-
-        ### YOUR CODE HERE:
-        raise NotImplementedError
-        ### END YOUR CODE
+        numgrad = (f_plus_h - f_minus_h) / (2 * h)
 
         # Compare gradients
-        assert_allclose(numgrad, grad[ix], rtol=1e-5,
+        assert_allclose(numgrad, grad[ix], rtol=1e-3,
                         err_msg=f"Gradient check failed for {gradient_text}.\n"
                                 f"First gradient error found at index {ix} in the vector of gradients\n"
                                 f"Your gradient: {grad[ix]} \t Numerical gradient: {numgrad}")
@@ -49,6 +43,7 @@ def gradcheck_naive(f, x, gradient_text=""):
         it.iternext()  # Step to next dimension
 
     print("Gradient check passed!")
+
 
 
 def test_gradcheck_basic():
@@ -65,18 +60,16 @@ def test_gradcheck_basic():
 
 
 def your_gradcheck_test():
-    """
-    Use this space add any additional sanity checks by running:
-        python q2_gradcheck.py
-    This function will not be called by the autograder, nor will
-    your additional tests be graded.
-    """
-    print("Running your sanity checks...")
-    ### YOUR OPTIONAL CODE HERE
-    pass
-    ### END YOUR CODE
+    # pass
+    d = 4
+    quad = lambda x: (np.sum(x ** d), d*(x**(d-1)))
+    print("Running sanity checks...")
+    gradcheck_naive(quad, np.array(123.456))       # scalar test
+    gradcheck_naive(quad, np.random.randn(3,))     # 1-D test
+    gradcheck_naive(quad, np.random.randn(4, 5))   # 2-D test
+    print()
 
 
 if __name__ == "__main__":
-    test_gradcheck_basic()
+    # test_gradcheck_basic()
     your_gradcheck_test()
